b2fa5c4065660e7f901b9f66bf598dfce71cf462
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-07-17 14:13:36 +0530

    handle update nav

--- src/components/MarketUpdate/UpdateNav.jsx/Updatenav.jsx
+++ src/components/MarketUpdate/UpdateNav.jsx/Updatenav.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Updatenav.module.css"
 import search from "../../../assets/search.png"
 
@@ -13,13 +13,22 @@ const navItems = [
 ]
 
 function Updatenav() {
+  const [highlighted , setHighlighted] = useState(0);
+
+  const handleItemClick = (index) =>{
+    setHighlighted(index)
+  }
   return (
     <div className={styles.updateNavContainer}>
       <div className={styles.navItems}>
         {
           navItems.map((item, index) => {
             return (
-              <div className={`${styles.navItem} ${index === 0 ? styles.firstNavItem : ""}`} key={index}>
+              <div className={`${styles.navItem} ${index==highlighted ? styles.highlightedItem : ""}`} key={index}
+              onClick={()=>{
+                handleItemClick(index)
+              }}
+              >
                 {item}
               </div>
             )
